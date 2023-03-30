d583a431db5857b9b3e99b0deb195ea1a3f15dcf
Gabe Jimenez <jimenezgabriel12@gmail.com>
2023-03-30 12:45:12 -0600

    Add functionality to es6 pattern functions

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,29 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.keyNum = 0;
+    this.storage = {};
+  }
+
+  enqueue(value) {
+    this.keyNum++;
+    this.storage[this.keyNum] = value;
+  }
+
+  dequeue() {
+    if (this.keyNum > 0) {
+      var result = this.storage[1];
+      delete this.storage[1];
+      for (var i in this.storage) {
+        this.storage[i - 1] = this.storage[this.keyNum]
+      }
+      this.keyNum--;
+      return result;
+    }
+  }
+
+  size() {
+    return this.keyNum;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,29 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.keyNum = 0;
+    this.storage = {};
+  }
+
+  pop() {
+    if (this.keyNum > 0) {
+      var result = this.storage[this.keyNum];
+      delete this.storage[this.keyNum];
+      for (var i in this.storage) {
+        this.storage[i - 1] = this.storage[i];
+      }
+      this.keyNum--;
+      return result;
+    }
+  }
+
+  push(value) {
+    this.keyNum++;
+    this.storage[this.keyNum] = value;
+  }
+
+  size() {
+    return this.keyNum;
   }
 
 }
