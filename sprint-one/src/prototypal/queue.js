f557e0bac9d4fa965b1863df360a25d3a9e9d195
Gabe Jimenez <jimenezgabriel12@gmail.com>
2023-03-30 11:46:35 -0600

    Add functionality to Stack and Queue prototypal functions

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,35 @@
-var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-};
 
-var queueMethods = {};
+ var Queue = function() {
+//   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+//   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.storage = {};
+  newQueue.keyNum = 0;
+  return newQueue;
+ };
 
+ queueMethods = {};
 
+queueMethods.enqueue = function (value) {
+  this.keyNum++;
+  this.storage[this.keyNum] = value;
+}
+
+queueMethods.dequeue = function () {
+  if (this.keyNum > 0) {
+    var result = this.storage[1];
+    delete this.storage[1];
+    for (var x in this.storage) {
+      this.storage[x - 1] = this.storage[x];
+    }
+    this.keyNum--;
+    return result;
+  }
+}
+
+queueMethods.size = function () {
+  return this.keyNum;
+}
+
+// //npm v16.19.0
+// //NVM 0.39.1
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  newStack.keyNum = 0;
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.pop = function () {
+  if (this.keyNum > 0) {
+    var result = this.storage[this.keyNum];
+    delete this.storage[this.keyNum];
+    this.keyNum--;
+    return result;
+  }
+}
 
+stackMethods.push = function (value) {
+  this.keyNum++;
+  this.storage[this.keyNum] = value;
+}
+
+stackMethods.size = function () {
+  return this.keyNum;
+}
