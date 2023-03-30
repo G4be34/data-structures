8f3f8197f59d3c85dc603a0181c0bfae748d8bf7
Gabe Jimenez <jimenezgabriel12@gmail.com>
2023-03-30 12:17:00 -0600

    Add functionality to Queue/Stack pseudoclassical patterns

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.keyNum = 0;
 };
 
+Queue.prototype.enqueue = function (value) {
+  this.keyNum++;
+  this.storage[this.keyNum] = value;
+}
 
+Queue.prototype.dequeue = function () {
+  if (this.keyNum > 0) {
+    var result = this.storage[1];
+    delete this.storage[1];
+    for (var i in this.storage) {
+      this.storage[i - 1] = this.storage[i];
+    }
+    this.keyNum--;
+    return result;
+  }
+}
+
+Queue.prototype.size = function () {
+  return this.keyNum;
+}
+
+var newQueue = new Queue();
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.keyNum = 0;
 };
 
+Stack.prototype.push = function (value) {
+  this.keyNum++;
+  this.storage[this.keyNum] = value;
+}
 
+Stack.prototype.pop = function () {
+  if (this.keyNum > 0) {
+    var result = this.storage[this.keyNum];
+    delete this.storage[this.keyNum];
+    for (var i in this.storage) {
+      this.storage[i - 1] = this.storage[i];
+    }
+    this.keyNum--;
+    return result;
+  }
+}
+
+Stack.prototype.size = function () {
+  return this.keyNum;
+}
+
+var newStack = new Stack();
