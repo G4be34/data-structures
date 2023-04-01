529ade6652db91883cb6fc272c4699dca9cd44e9
Gabe Jimenez <jimenezgabriel12@gmail.com>
2023-04-01 12:23:15 -0600

    Add functionality to add, contains, remove to Set data structure

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,29 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // if this._storage.item doesn't exist,
+  if (!this._storage[item]) {
+    this._storage[item] = 0;
+  }
+  this._storage[item]++;
 };
 
 setPrototype.contains = function(item) {
+  var result = false;
+  if (this._storage[item]) {
+    result = true;
+  }
+  return result;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
