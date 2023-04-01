960c17135538e2705392bc16be0d848404f60bc2
Gabe Jimenez <jimenezgabriel12@gmail.com>
2023-03-31 23:16:16 -0600

    Add functionality to addNode, contains and addEdge functions in graph data structure

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,14 +2,28 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.allNodes = {1: {value: 1, connectedNodes: {2 : 1, 3 : 1,}}}
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.allNodes[node] = {
+    value: node,
+    connectedNodes: {}
+  };
 };
+
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  var result = false;
+  for (var i in this.allNodes) {
+    if (this.allNodes[i].value === node) {
+      result = true;
+      break;
+    }
+  }
+  return result;
 };
 
 // Removes a node from the graph.
@@ -20,8 +34,14 @@ Graph.prototype.removeNode = function(node) {
 Graph.prototype.hasEdge = function(fromNode, toNode) {
 };
 
+// this.allNodes = {value: 1, connectedNodes: {2 : 1, 3 : 1}}
+
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  var from = this.allNodes[fromNode].connectedNodes;
+  from[toNode] = 1;
+  var to = this.allNodes[toNode].connectedNodes;
+  to[fromNode] = 1;
 };
 
 // Remove an edge between any two specified (by value) nodes.
